6b5aa8732886496339d24c3910d39482bf1a428b
Kent <kent.kadim@dkatalis.com>
2023-07-18 15:55:02 +0800

    test: adds edge cases with network stubs

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -1,5 +1,21 @@
 import stats from "../fixtures/stats.ts";
 describe("know your starter tests", () => {
+  before(() => {
+    // runs once before all tests in the block
+  });
+
+  beforeEach(() => {
+    // runs before each test in the block
+  });
+
+  afterEach(() => {
+    // runs after each test in the block
+  });
+
+  after(() => {
+    // runs once after all tests in the block
+  });
+
   const appURL = Cypress.env("local") as string;
 
   const checkStats = (
@@ -39,5 +55,41 @@ describe("know your starter tests", () => {
 
         checkStats(stats);
       });
+  });
+
+  it("should show error message when the API does not return expected result", () => {
+    cy.intercept(
+      {
+        method: "GET", // Route all GET requests
+        url: "/api/v2/pokemon/*", // that have a URL that matches the pattern
+      },
+      {},
+    ).as("getPokemonDetail"); // and assign an alias
+
+    cy.visit(appURL);
+
+    cy.contains("charmander")
+      .click()
+      .then(() => {
+        cy.contains("oops, something went wrong");
+      });
+  });
+
+  it("should show loading state when fetching data", () => {
+    cy.intercept(
+      {
+        method: "GET", // Route all GET requests
+        url: "/api/v2/pokemon/*", // that have a URL that matches the pattern
+      },
+      {
+        delay: 1000,
+        fixture: "charmander.json",
+      },
+    ).as("getPokemonDetail"); // and assign an alias
+
+    cy.visit(appURL);
+    cy.contains("charmander").click();
+    cy.contains("loading...");
+    cy.wait("@getPokemonDetail");
   });
 });
